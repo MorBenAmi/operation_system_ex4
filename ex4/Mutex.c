315b89716ce7b67cee3c425028cbfb431210d26b
MorBenAmi <mor.bami@gmail.com>
2017-01-07 13:00:16 +0200

    Remove mutex num functions from mutex files

--- ex4/Mutex.c
+++ ex4/Mutex.c
@@ -29,24 +29,6 @@ BOOL lock_mutex(char* mutex_name)
 	printf("unable to gain ownership on mutex: %s\n",mutex_name);
 	return FALSE;
 }
-//this function first convert the num to string and then uses the function "lock_mutex" to lock the mutex//
-BOOL lock_mutex_num(unsigned long int mutex_num)
-{
-	BOOL result;
-	char* mutex_name;
-	int n = _snprintf(NULL,0,"%ul", mutex_num);
-	mutex_name = (char*)malloc(sizeof(char)*n+1);
-	if(mutex_name==NULL)
-	{
-		printf("Failed to malloc mutex name. Error code: 0x%x\n", GetLastError());
-		write_log("Failed to malloc mutex name. Error code: 0x%x\n", GetLastError());
-		return FALSE;
-	}
-	_snprintf(mutex_name, n+1, "%ul", mutex_num);
-	result = lock_mutex(mutex_name);
-	free(mutex_name);
-	return result;
-}
 
 BOOL unlock_mutex(char* mutex_name)
 {
@@ -69,24 +51,6 @@ BOOL unlock_mutex(char* mutex_name)
 	return ReleaseMutex(mutex);
 }
 
-BOOL unlock_mutex_num(unsigned long int mutex_num)
-{
-	BOOL result;
-	char* mutex_name;
-	int n = _snprintf(NULL,0,"%ul", mutex_num);
-	mutex_name = (char*)malloc(sizeof(char)*n+1);
-	if(mutex_name==NULL)
-	{
-		printf("Failed to malloc mutex name. Error code: 0x%x", GetLastError());
-		write_log("Failed to malloc mutex name. Error code: 0x%x", GetLastError());
-		return FALSE;
-	}
-	_snprintf(mutex_name, n+1, "%ul", mutex_num);
-	result = unlock_mutex(mutex_name);
-	free(mutex_name);
-	return result;
-}
-
 void close_mutex(char* mutex_name)
 {
 	HANDLE mutex = NULL;
@@ -100,20 +64,4 @@ void close_mutex(char* mutex_name)
 	SetLastError(last_error);
 
 	CloseHandle(mutex);
-}
-
-void close_mutex_num(unsigned long int mutex_num)
-{
-	BOOL result;
-	char* mutex_name;
-	int n = _snprintf(NULL,0,"%ul", mutex_num);
-	mutex_name = (char*)malloc(sizeof(char)*n+1);
-	if(mutex_name==NULL)
-	{
-		printf("Failed to malloc mutex name. Error code: 0x%x", GetLastError());
-		return;
-	}
-	_snprintf(mutex_name, n+1, "%ul", mutex_num);
-	close_mutex(mutex_name);
-	free(mutex_name);
 }
--- ex4/Mutex.h
+++ ex4/Mutex.h
@@ -7,12 +7,9 @@
 #include "Events.h"
 
 BOOL lock_mutex(char* mutex_name);
-BOOL lock_mutex_num(unsigned long int mutex_num);
 
 BOOL unlock_mutex(char* mutex_name);
-BOOL unlock_mutex_num(unsigned long int mutex_num);
 
 void close_mutex(char* mutex_name);
-void close_mutex_num(unsigned long int mutex_num);
 
 #endif
