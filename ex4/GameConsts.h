a42f6e0af6846d8e87fa2f7f47d142ad69edaa59
MorBenAmi <mor.bami@gmail.com>
2017-01-26 21:47:17 +0200

    Add and rename constans

--- ex4/GameConsts.h
+++ ex4/GameConsts.h
@@ -13,7 +13,14 @@
 #define MAX_USER_NOT_EXIST_MESSAGE_LENGTH 1000 //todo: change to real value
 
 #define MAX_SIZE_OF_USERNAME_MESSAGE 41
-#define MAX_MESSAGE_SIZE 80
+#define MAX_USER_MESSAGE_LENGTH 80
 
+#define COMMAND_MESSAGE "message"
+#define COMMAND_PLAY "play"
+#define COMMAND_PLAYERS "players"
+#define COMMAND_BROADCAST "broadcast"
+
+#define COMMAND_MESSAGE_MIN_ARGS 3
+#define COMMAND_BROADCAST_MIN_ARGS 2
 
 #endif
