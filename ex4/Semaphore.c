06e136efc2a27569bbc8df6c15855fb1a83d4ec3
Limor Mendelzburg <limor113@gmail.com>
2017-01-27 02:36:05 +0200

    fix semaphore

--- ex4/Semaphore.c
+++ ex4/Semaphore.c
@@ -2,12 +2,21 @@
 
 HANDLE CreateSemaphoreSimple(char* name)
 {
+	HANDLE created_semaphore;
+	DWORD last_error = 0;
 	char unique_semaphore_name[MAX_SEMAPHORE_LENGTH];
 
 	memset(unique_semaphore_name, '\0', MAX_SEMAPHORE_LENGTH);
 	sprintf(unique_semaphore_name, "%d_%s", GetCurrentProcessId(), name);
 
-	return CreateSemaphore(NULL, 0, 1, (LPCWSTR)unique_semaphore_name);
+	last_error = GetLastError();
+
+	created_semaphore = CreateSemaphore(NULL, 0, 1, (LPCWSTR)unique_semaphore_name);
+	if(created_semaphore == NULL)
+		return NULL;
+
+	SetLastError(last_error);
+	return created_semaphore;
 }
 
 void ReleaseSemaphoreSimple(HANDLE semaphore) 
