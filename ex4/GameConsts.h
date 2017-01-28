eb35c1c632d59a7c64df347f2eb9c98c99f6c18c
Limor Mendelzburg <limor113@gmail.com>
2017-01-28 18:39:45 +0200

    make game consts correct..

--- ex4/GameConsts.h
+++ ex4/GameConsts.h
@@ -6,14 +6,15 @@
 #define MAX_USER_NAME_MESSAGE_LENGTH (10+MAX_USER_NAME_LENGTH) //username=<username>\n
 #define MAX_WELCOME_MESSAGE_LENGTH (MAX_USER_NAME_LENGTH+20) // <username> you game piece is <game piece>\n
 #define MAX_PLAYER_JOINED_MESSAGE_LENGTH (35+MAX_USER_NAME_LENGTH) // New player joined the game: <username> <game piece>\n
-#define MAX_PLAYERS_LIST_MESSAGE_LENGTH (MAX_NUM_OF_PLAYERS*(MAX_USER_NAME_LENGTH + 4) + 1) // <user1>-<game_piece>, <user2>-<game_piece>, <user3>-<game_piece>, <user4>-<game_piece>.\n
+#define MAX_PLAYERS_LIST_MESSAGE_LENGTH (MAX_NUM_OF_PLAYERS*(MAX_USER_NAME_LENGTH + 4) + 3) // <user1>-<game_piece>, <user2>-<game_piece>, <user3>-<game_piece>, <user4>-<game_piece>.\n
 #define MAX_USER_NOT_EXIST_MESSAGE_LENGTH (MAX_USER_NAME_LENGTH+35) // User <username> doesn't exist in the game.\n
 #define MAX_PLAYER_TURN_MESSAGE_LENGTH (MAX_USER_NAME_LENGTH+30) // It is now <username>'s turn to play.\n
 
-#define MAX_PRIVATE_MESSAGE_LENGTH 1000
-#define MAX_BROADCAST_MESSAGE_LENGTH 1000
+#define MAX_MESSAGE_LENGTH 80
+#define MAX_PRIVATE_MESSAGE_LENGTH (MAX_USER_NAME_LENGTH+MAX_MESSAGE_LENGTH+30) // Private message from <username>: <message>
+#define MAX_BROADCAST_MESSAGE_LENGTH (MAX_USER_NAME_LENGTH+MAX_MESSAGE_LENGTH+20) // Broadcast from <username>: <message>
 
-#define MAX_COMMAND_LENGTH 1000
+#define MAX_COMMAND_LENGTH 200 // longer than the max command(players message response) just to be on the safe side..
 
 #define MAX_SIZE_OF_USERNAME_MESSAGE 41
 #define MAX_USER_MESSAGE_LENGTH 80
